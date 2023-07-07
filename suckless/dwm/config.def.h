bbe157f97a39bb00694e3be0045a05f9e131f2b0
Nabeel Ali <mail2nabeelali@gmail.com>
2023-07-07 13:48:40 +0500

    fixed

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -30,47 +30,47 @@ static const char dmenufont[]            = "JetBrains Mono Nerd Font:size=12";
 
 static char c000000[]                    = "#000000"; // placeholder value
 
-#include "/home/icon/.cache/wal/colors-wal-dwm.h";
+//  #include "/home/icon/.cache/wal/colors-wal-dwm.h";
 
-// static char normfgcolor[]                = "#bbbbbb";
-// static char normbgcolor[]                = "#222222";
-// static char normbordercolor[]            = "#444444";
-// static char normfloatcolor[]             = "#db8fd9";
+static char normfgcolor[]                = "#bbbbbb";
+static char normbgcolor[]                = "#222222";
+static char normbordercolor[]            = "#444444";
+static char normfloatcolor[]             = "#db8fd9";
 
-// static char selfgcolor[]                 = "#eeeeee";
-// static char selbgcolor[]                 = "#005577";
-// static char selbordercolor[]             = "#005577";
-// static char selfloatcolor[]              = "#005577";
+static char selfgcolor[]                 = "#eeeeee";
+static char selbgcolor[]                 = "#005577";
+static char selbordercolor[]             = "#005577";
+static char selfloatcolor[]              = "#005577";
 
-// static char titlenormfgcolor[]           = "#bbbbbb";
-// static char titlenormbgcolor[]           = "#222222";
-// static char titlenormbordercolor[]       = "#444444";
-// static char titlenormfloatcolor[]        = "#db8fd9";
+static char titlenormfgcolor[]           = "#bbbbbb";
+static char titlenormbgcolor[]           = "#222222";
+static char titlenormbordercolor[]       = "#444444";
+static char titlenormfloatcolor[]        = "#db8fd9";
 
-// static char titleselfgcolor[]            = "#eeeeee";
-// static char titleselbgcolor[]            = "#005577";
-// static char titleselbordercolor[]        = "#005577";
-// static char titleselfloatcolor[]         = "#005577";
+static char titleselfgcolor[]            = "#eeeeee";
+static char titleselbgcolor[]            = "#005577";
+static char titleselbordercolor[]        = "#005577";
+static char titleselfloatcolor[]         = "#005577";
 
-// static char tagsnormfgcolor[]            = "#bbbbbb";
-// static char tagsnormbgcolor[]            = "#222222";
-// static char tagsnormbordercolor[]        = "#444444";
-// static char tagsnormfloatcolor[]         = "#db8fd9";
+static char tagsnormfgcolor[]            = "#bbbbbb";
+static char tagsnormbgcolor[]            = "#222222";
+static char tagsnormbordercolor[]        = "#444444";
+static char tagsnormfloatcolor[]         = "#db8fd9";
 
-// static char tagsselfgcolor[]             = "#eeeeee";
-// static char tagsselbgcolor[]             = "#005577";
-// static char tagsselbordercolor[]         = "#005577";
-// static char tagsselfloatcolor[]          = "#005577";
+static char tagsselfgcolor[]             = "#eeeeee";
+static char tagsselbgcolor[]             = "#005577";
+static char tagsselbordercolor[]         = "#005577";
+static char tagsselfloatcolor[]          = "#005577";
 
-// static char hidnormfgcolor[]             = "#005577";
-// static char hidselfgcolor[]              = "#227799";
-// static char hidnormbgcolor[]             = "#222222";
-// static char hidselbgcolor[]              = "#222222";
+static char hidnormfgcolor[]             = "#005577";
+static char hidselfgcolor[]              = "#227799";
+static char hidnormbgcolor[]             = "#222222";
+static char hidselbgcolor[]              = "#222222";
 
-// static char urgfgcolor[]                 = "#bbbbbb";
-// static char urgbgcolor[]                 = "#222222";
-// static char urgbordercolor[]             = "#ff0000";
-// static char urgfloatcolor[]              = "#db8fd9";
+static char urgfgcolor[]                 = "#bbbbbb";
+static char urgbgcolor[]                 = "#222222";
+static char urgbordercolor[]             = "#ff0000";
+static char urgfloatcolor[]              = "#db8fd9";
 
 
 
